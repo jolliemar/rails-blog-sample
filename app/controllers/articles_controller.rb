038aea922e52638a64ff87ce05303a77eb33c237
Jollie Mar Carino Gutang <jolliemar@gmail.com>
2019-11-06 15:17:34 +0900

    added edit, and update

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,6 +11,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
 =begin 
     Article class defined in models/article.rb 
@@ -21,6 +25,16 @@ class ArticlesController < ApplicationController
       redirect_to @article
     else
       render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if(@article.update(article_params))
+      redirect_to @article
+    else
+      render 'edit'
     end
   end
 
